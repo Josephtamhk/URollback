5417fcbfcbe49a5d5587807dda6076823174144f
christides11 <christides11@hotmail.com>
2020-02-06 11:27:09 -0500

    Created URollbackWorld script.

---
+++ Assets/URollback/Core/URollbackWorld.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A URollbackWorld keeps track of the data of the sim entities currently
+/// in the rollback session, and handles restoring their data and saving it.
+/// </summary>
+namespace URollback.Core
+{
+    public class URollbackWorld
+    {
+
+    }
+}
